1ae3bf3bdac1bf5a6cffb42d01dfc3c2f99696ee
sonal kumar mandal <152254912+SONALkumarmandal@users.noreply.github.com>
2025-12-07 22:17:36 +0530

    backend success!

--- server/src/routes/noteRoutes.ts
+++ server/src/routes/noteRoutes.ts
@@ -1,5 +1,9 @@
 import { Router } from "express";
-import { createNote } from "../controllers/utils.controller";
+import { getAllNotes,
+  getNoteById,
+  createNote,
+  updateNote,
+  deleteNote } from "../controllers/utils.controller";
 import { getAppHealth, getAppInfo, getEnv } from "../controllers/info.controller";
 
 const router =Router()
@@ -8,7 +12,11 @@ router.get("/aapinfo" , getAppInfo)
 router.get("/apphealth" , getAppHealth)
 router.get("/getenv" ,getEnv)
 
-router.post("/create" , createNote)
+router.get("/", getAllNotes);
+router.get("/:id", getNoteById);
+router.post("/", createNote);
+router.put("/:id", updateNote);
+router.delete("/:id", deleteNote);
 
 
 export default router
